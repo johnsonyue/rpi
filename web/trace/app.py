d48d47e8307a6a2a6b606bb99cbfb06a17a2dd49
johnsonyue <johnsonyuehit@163.com>
2017-11-05 18:25:37 +0800

    barebone

--- web/trace/app.py
+++ web/trace/app.py
@@ -1,10 +1,12 @@
 import os
 import time
+import datetime
 import json
 import BaseHTTPServer
 from SocketServer import ThreadingMixIn
 
 import cgi
+import subprocess
 
 class Server(BaseHTTPServer.HTTPServer):
 	def __init__(self, (HOST_NAME, PORT_NUMBER), handler, config):
@@ -44,7 +46,7 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			
 	def do_POST(self):
 		action = self.path.replace('/','')
-		valid_action = ["trace"]
+		valid_action = ["paste"]
 		if ( action not in valid_action ):
 			self.send_response(505)
 			self.end_headers()
@@ -59,11 +61,25 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 			'CONTENT_TYPE':self.headers['Content-Type'],
 		})
 
-		if ( action == "trace" ):
-			if post.has_key("ip"):
-				ip = post["ip"].value
-				helper = db.db_helper()
-				result = helper.query_ip_neighbours(ip)
+		config = json.loads(open("config.json").read())
+		data_dir = config["data"]["root_dir"]
+		if ( action == "paste" ):
+			if post.has_key("target"):
+				target = post["target"].value
+
+				user = "anonymous"
+				date = datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
+				output_dir = data_dir+"/"+user
+				target_file_path = output_dir+"/"+user+"."+date+".target"
+
+				if not os.path.exists(output_dir):
+					os.makedirs(output_dir)
+				fp = open(target_file_path, 'wb')
+				fp.write(target)
+				fp.close()
+
+				h = subprocess.Popen(["./trace", target_file_path, output_dir], stdout=subprocess.PIPE)
+				result = h.stdout.read()
 				self.send_response(200)
 				self.end_headers()
 				self.wfile.write(result)
--- web/trace/ws.py
+++ web/trace/ws.py
@@ -5,6 +5,12 @@ import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
+import subprocess
+
+class WSApplication(tornado.web.Application):
+	def __init__(self, handlers, config):
+		tornado.web.Application.__init__(self, handlers)
+		self.config = config
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
@@ -20,15 +26,20 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 		return True
 
 	def scheduled(self):
+		user = "anonymous"
 		self.write_message("Hello,world!")
-		tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=5), self.scheduled) 
+		config = self.application.config
+		data_dir = config["data"]["root_dir"]
+		output_dir = data_dir+"/"+user
+
+		tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=1), self.scheduled) 
 
 if __name__ == '__main__':
 	config = json.loads(open("config.json").read())
 	HOST_NAME = config["app"]["host_name"]
 	WSPORT_NUMBER = config["app"]["wsport_number"]
 
-	application = tornado.web.Application([(r'/ws', WSHandler),])
+	application = WSApplication([(r'/ws', WSHandler),], config)
 	ws_server = tornado.httpserver.HTTPServer(application)
 	ws_server.listen(WSPORT_NUMBER)
 
